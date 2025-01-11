1eb4c9e373b365fd46082cf56f8f57e573bfef44
yairbecher <156304037+yairbecher@users.noreply.github.com>
2025-01-11 20:01:23 +0200

    firs commit

--- fetch_end_prep_functions.py
+++ fetch_end_prep_functions.py
@@ -4,6 +4,7 @@ import statistics as st
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
+columns_for_calc = ['Sales', 'Profit', 'Assets', 'Market Value']
 
 def prep_df(df: pd.DataFrame, columns_for_calc: list):
     for column in columns_for_calc:
@@ -32,12 +33,62 @@ def biuld_df_by_country(df: pd.DataFrame):
     return df
 
 
+def companys_per_country(df: pd.DataFrame):
+    contry = input('input cuontry for check: ')
+    df_spsific_country = df[df['Country'] == contry]
+    return df_spsific_country
+
+
+# def calc_statistic_by_companys(df: pd.DataFrame, columns_for_calc: list):
+#     action_list = ['mean', 'median', 'mode', 'range', 'standard_deviation']
+#     data_dickt = defaultdict(list)
+#     for action in action_list:
+#         data_dickt['action'].append(action)
+#         data_dickt['sales'].append()
+#         data_dickt['profit'].append()
+#         data_dickt['assets'].append()
+#         data_dickt['sales'].append()
+#         data_dickt['market_value'].append()
+#
+#
+#     return df_statistic
+
+def calc_statistic_by_companys(df: pd.DataFrame, columns_for_calc: list):
+    data_dickt = defaultdict(list)
+    action_list = ['avg', 'median', 'str']
+    for action in action_list:
+        data_dickt['calc_action'].append(action)
+    for column in columns_for_calc:
+        avg = df[column].mean()
+        median = df[column].median()
+        std =df[column].std()
+
+
+        data_dickt[column].append(avg)
+        data_dickt[column].append(median)
+        data_dickt[column].append(std)
+
+    df = pd.DataFrame(data_dickt)
+    return df
+
+
+file_path = '/Users/yairbecher/Yair staf/Python_learning/2000_largest_companys_data/Top 2000 Companies Financial Data 2024.csv'
+df = pd.read_csv(file_path)
+df_prep = prep_df(df, columns_for_calc)
+df_statistic = calc_statistic_by_companys(df_prep, columns_for_calc)
+# df_spsific_country = companys_per_country(df)
+df_by_country = biuld_df_by_country(df)
+print(df.shape)
 
 
 
 
 
 
+
+
+
+# Step 2: Count the number of companies per country
 country_counts = df['big_countrys'].value_counts()
 
 # Step 3: Create the bar chart
@@ -54,6 +105,7 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 # Show the chart
 plt.show()
+print("hii")
 
 
 
