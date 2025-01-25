5134e65a0eda784010fb667328f44fa12b90e098
Yair Becher <yairbecher@gmail.com>
2025-01-25 14:35:18 +0200

    add a investment feasibility index graph

--- plots_functions.py
+++ plots_functions.py
@@ -5,30 +5,13 @@ import statistics as st
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from scipy.stats import norm
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 def run_plots_analyss(df_by_country: pd.DataFrame, columns_for_calc: list, df_statistic_by_company: pd.DataFrame):
     profit_percentage_graph(df_by_country)
     distribution_by_country(df_by_country)
-
-
-
-
-def distribution_by_country(df: pd.DataFrame):
-
-    plt.figure(figsize=(30, 6))
-
-    sns.kdeplot(df['num_of_companys'], fill=True, color='blue', alpha=0.5)
-    plt.xticks(range(1, max(df['num_of_companys']), 10))
-    # plt.xticks(df['num_of_companys'], df['country'], rotation=90, fontsize=10)
-
-
-    plt.title('Density of Companies Across Countries')
-    plt.xlabel('Number of Companies')
-    plt.ylabel('Density')
-    plt.show()
+    investment_feasibility_index(df_by_country)
 
 
 def profit_percentage_graph(df: pd.DataFrame):
@@ -50,3 +33,44 @@ def profit_percentage_graph(df: pd.DataFrame):
     plt.grid(True)
     plt.show()
 
+
+def distribution_by_country(df: pd.DataFrame):
+
+    plt.figure(figsize=(30, 6))
+
+    sns.kdeplot(df['num_of_companys'], fill=True, color='blue', alpha=0.5)
+    plt.xticks(range(1, max(df['num_of_companys']), 10))
+    # plt.xticks(df['num_of_companys'], df['country'], rotation=90, fontsize=10)
+
+
+    plt.title('Density of Companies Across Countries')
+    plt.xlabel('Number of Companies')
+    plt.ylabel('Density')
+    plt.show()
+
+def investment_feasibility_index(df):
+    df['company_weight'] = np.log(df['num_of_companys'])
+    df['IPI'] = df['avg_profit_percentage'] * (df['profit'] / df['num_of_companys']) * df['company_weight']
+    df = df.sort_values(by='IPI', ascending=False)
+
+    plt.figure(figsize=(12, 10))
+    sns.barplot(x='country', y='IPI', data=df, palette='viridis')
+    plt.title('Investment Profitability Index by Country')
+    plt.xlabel('country')
+    plt.ylabel('Investment Profitability Index (IPI)')
+    plt.xticks(rotation=90)
+
+    formula = r"$\text{IPI} = \left(\frac{\text{Profit}}{\text{Sales}}\right) \times \left(\frac{\text{Profit}}{\text{Companies}}\right) \times \ln(\text{Companies})$"
+
+    plt.annotate(
+        formula,
+        xy=(0.85, 0.90),
+        xycoords='axes fraction',
+        fontsize=12,
+        color='black',
+        ha='right',
+        va='top',
+        bbox=dict(facecolor='white', alpha=0.5, edgecolor='black')
+    )
+
+    plt.show()
