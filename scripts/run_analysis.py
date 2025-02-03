bedad2b04d5a95366139c3f958b2103aa3cd72c3
Yair Becher <yairbecher@gmail.com>
2025-02-03 17:30:27 +0200

    change os file name

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -6,8 +6,8 @@ from plots_functions import run_plots_analysis
 
 
 
-file_path = os.environ.get('file_path')
-df = pd.read_csv(file_path)
+impot_file = os.environ.get('impot_file')
+df = pd.read_csv(impot_file)
 
 
 columns_for_calc = ['sales', 'profit', 'avg_profit_percentage', 'assets', 'market_value']
@@ -18,7 +18,7 @@ df_prep = prep_df(df, columns_for_calc)
 df_statistic_by_company = calc_df_statistic(df_prep, columns_for_calc)
 df_by_country = biuld_df_by_country(df_prep)
 df_statistic_by_country = calc_df_statistic(df_by_country, columns_for_calc)
-df_spsific_country = fetch_specific_country(df_prep)
+# df_spsific_country = fetch_specific_country(df_prep)
 
 
 plots = run_plots_analysis(df_by_country, columns_for_calc, df_statistic_by_company)
