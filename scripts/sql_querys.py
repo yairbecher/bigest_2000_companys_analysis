4d206dbbba75aacec466f9c6647d4364fa8901d6
Yair Becher <yairbecher@gmail.com>
2025-02-07 12:41:42 +0200

    add more querys

--- scripts/sql_querys.py
+++ scripts/sql_querys.py
@@ -6,7 +6,7 @@ from dfs_functions import prep_df
 
 columns_for_calc = ['sales', 'profit', 'avg_profit_percentage', 'assets', 'market_value']
 
-impot_file = os.environ.get('impot_file')
+impot_file = os.environ.get('inpot_file')
 df = pd.read_csv(impot_file)
 df = prep_df(df, columns_for_calc)
 
@@ -50,7 +50,7 @@ def best_company(conn):
     # FROM countries
     # group by name"""
 
-    query = """SELECT name, DENSE_RANK() OVER(ORDER BY total_rank ASC) as ranke
+    query = """SELECT name, DENSE_RANK() OVER(ORDER BY total_rank DESC) AS ranke
     FROM (SELECT name, 
         (DENSE_RANK() OVER(ORDER BY sales ASC) +
         DENSE_RANK() OVER(ORDER BY profit ASC) +
@@ -66,3 +66,68 @@ def best_company(conn):
 df_renk = best_company(conn)
 
 
+# Retrieve the top 3 most profitable companies for each country, ordered by profit.
+
+def top_3_profitable_companies(conn):
+    query = """with ramk_tabel as ( 
+               SELECT country, name, RANK() OVER (PARTITION BY country ORDER BY profit DESC) AS rank 
+               FROM countries)
+    select country, GROUP_CONCAT(name, ', ') AS top_companies
+    from ramk_tabel
+    where rank < 4
+    group by country
+    """
+
+    result = pd.read_sql_query(query, conn)
+    print(result)
+    return result
+
+df_top_3_profitable_companies = top_3_profitable_companies(conn)
+
+
+
+# Find the companies where the market value is less than their total assets. Rank them by the ratio of market value to assets.
+
+def market_value_and_assets_Comparison(conn):
+    query = """with negative_market_value as (select name, market_value - assets as gap from countries where gap > 0)
+    select  NAME, RANK() OVER(ORDER BY GAP DESC) as gap from negative_market_value"""
+
+    result = pd.read_sql_query(query, conn)
+    print(result)
+    return result
+
+market_value_and_assets_Comparison = market_value_and_assets_Comparison(conn)
+
+# Identify the companies with the highest sales but a below-average profit margin.
+
+def highest_sales_but_low_profit(conn):
+    query = """select name, sales, profit
+    from countries
+    where profit > (SELECT AVG(profit) FROM countries)
+    order by sales"""
+
+    result = pd.read_sql_query(query, conn)
+    print(result)
+    return result
+
+df_highest_sales_but_low_profit = highest_sales_but_low_profit(conn)
+
+
+# Determine what percentage of a country's total market value is held by its top 5 companies.
+
+def best_compais_market_value_percentage(conn):
+    # query = """with 5_best_companies as (select name, market_value rank() over(order by market_value desc) as rank
+    # from countries
+    # QUALIFY rank < 5) select * from 5_best_companies"""
+
+    query = """   SELECT
+                name, market_value
+                
+                FROM countries
+                where (select RANK() OVER(ORDER BY market_value DESC) as rank) < 299800000000;"""
+
+    result = pd.read_sql_query(query, conn)
+    print(result)
+    return result
+
+df_best_compais_market_value_percentage = best_compais_market_value_percentage(conn)
