5d771ab1544433f3e68540184ea3dbefad1b36a2
Yair Becher <yairbecher@gmail.com>
2025-02-10 14:04:19 +0200

    cosmetic changes

--- scripts/Market_value_prediction.py
+++ scripts/Market_value_prediction.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 from dfs_functions import prep_df
 
+
 impot_file = os.environ.get('inpot_file')
 df = pd.read_csv(impot_file, index_col=0)
 
@@ -15,44 +16,24 @@ columns_for_calc = ['sales', 'profit', 'avg_profit_percentage', 'assets', 'marke
 df = prep_df(df, columns_for_calc)
 
 
-#To correctly divide the countries in half according to market value.
-# Understand how to properly treat countries that don't appear much, because putting it in "other" can affect things that I'm not sure I understand
+df_sorted = df.sort_values(by='market_value', ascending=False)
 
-
-###First model regardless of country###
-
-# # Encode categorical variables ('country')
-# preprocessor = ColumnTransformer(
-#     transformers=[
-#         ('country', OneHotEncoder(), ['country'])],remainder='passthrough')
-#
-# df_sorted = df.sort_values(by='market value', ascending=False)
-#
-# # Split the data by alternating large and small companies
-# X_train = df_sorted.iloc[::2].drop(columns=['market value'])  # Select every other row starting from the largest
-# X_test = df_sorted.iloc[1::2].drop(columns=['market value'])  # Select every other row starting from the second largest
-#
-# y_train = df_sorted.iloc[::2]['market value']
-# y_test = df_sorted.iloc[1::2]['market value']
-
-
-# Split the data into features and target
-y = df['market_value']  # Target
-df_with_country = df.drop(columns=['name'])  # Features including country
-df_without_country = df.drop(columns=['name', 'country'])  # Features excluding country
+y = df['market_value']
+df_with_country = df.drop(columns=['name'])
+df_without_country = df.drop(columns=['name', 'country'])
 
 df['country_encoded'] = df['country'].map(df['country'].value_counts())
 
 
-X_train_w = df_with_country.iloc[::2].drop(columns=['market_value'])  # Select every other row starting from the largest
-X_test_w = df_with_country.iloc[1::2].drop(columns=['market_value'])  # Select every other row starting from the second largest
+X_train_w = df_with_country.iloc[::2].drop(columns=['market_value'])
+X_test_w = df_with_country.iloc[1::2].drop(columns=['market_value'])
 
 y_train_w = df_with_country.iloc[::2]['market_value']
 y_test_w = df_with_country.iloc[1::2]['market_value']
 
 
-X_train_n = df_without_country.iloc[::2].drop(columns=['market_value'])  # Select every other row starting from the largest
-X_test_n = df_without_country.iloc[1::2].drop(columns=['market_value'])  # Select every other row starting from the second largest
+X_train_n = df_without_country.iloc[::2].drop(columns=['market_value'])
+X_test_n = df_without_country.iloc[1::2].drop(columns=['market_value'])
 
 y_train_n = df_without_country.iloc[::2]['market_value']
 y_test_n = df_without_country.iloc[1::2]['market_value']
@@ -61,8 +42,7 @@ y_test_n = df_without_country.iloc[1::2]['market_value']
 # # Split the data into training and testing sets (50/50)
 # X_train_w, X_test_w, y_train, y_test = train_test_split(X_with_country, y, test_size=0.5, random_state=42)
 # X_train_n, X_test_n, y_train, y_test = train_test_split(X_without_country, y, test_size=0.5, random_state=42)
-#
-# # Define the preprocessing for the "country" column (only needed in the "with country" model)
+
 preprocessor = ColumnTransformer(
     transformers=[
         ('country', OneHotEncoder(handle_unknown='ignore'), ['country'])  # Ignore unknown categories
@@ -99,7 +79,7 @@ r2_w = r2_score(y_test_w, y_pred_w)
 mae_n = mean_absolute_error(y_test_n, y_pred_n)
 r2_n = r2_score(y_test_n, y_pred_n)
 
-# Output the results
+
 print("Model with Country Included:")
 print(f"Mean Absolute Error: {mae_w}")
 print(f"R-squared: {r2_w}\n")
